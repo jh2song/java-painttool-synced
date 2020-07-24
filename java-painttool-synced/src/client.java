ec688ef6fee447a84260b5c0909d159437360760
jh2song <spec0003@naver.com>
2020-07-24 15:01:09 +0900

    base 01

---
+++ java-painttool-synced/src/client.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class client extends JFrame {
+	public client() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new client();
+	}
+}
---
+++ java-painttool-synced/src/server.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class server extends JFrame {
+	public server() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new server();
+	}
+}
